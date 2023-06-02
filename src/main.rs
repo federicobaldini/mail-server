ec508001daab1d1aa13f7ad21b20dd11fe06c335
Federico Baldini <federico.baldini7@gmail.com>
2023-06-02 10:39:07 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+// Handler function for the HTTP request.
+async fn greet(request: HttpRequest) -> impl Responder {
+  let name: &str = request.match_info().get("name").unwrap_or("World");
+  format!("Hello {}!", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+  // Create an HTTP server and configure routes.
+  HttpServer::new(|| {
+    App::new()
+      .route("/", web::get().to(greet)) // Route for the root path.
+      .route("/{name}", web::get().to(greet)) // Route with a dynamic parameter.
+  })
+  .bind("127.0.0.1:8000")? // Bind the server to a specific address and port.
+  .run() // Run the server.
+  .await // Wait for the server to finish.
 }
