a1d0c747c4d290ed434d2482fc5ff80ba68dbe23
Federico Baldini <federico.baldini7@gmail.com>
2023-07-01 12:34:52 +0200

    Define requirements for email newsletter subscription
    Added tests to ensure that the server responds with a "200 OK" if a valid name and email pair is provided, and a "400 BAD REQUEST" if either the name or email is missing.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,29 +3,6 @@ use std::net::TcpListener;
 use actix_web::dev::Server;
 use reqwest::{Client, Response};
 
-/// Asynchronous test function to check if the health check endpoint works.
-#[tokio::test]
-async fn health_check_works() {
-  // Start the application server in the background.
-  let address: String = spawn_app();
-
-  // Create a new reqwest client.
-  let client: Client = reqwest::Client::new();
-
-  // Send a GET request to the '/health_check' endpoint.
-  let response: Response = client
-    .get(&format!("{}/health_check", &address))
-    .send()
-    .await
-    .expect("Failed to execute request.");
-
-  // Assert that the response status is successful (2xx).
-  assert!(response.status().is_success());
-
-  // Assert that the response content length is 0 (no body).
-  assert_eq!(Some(0), response.content_length());
-}
-
 /// Spawns the application server and returns the server address.
 ///
 /// ### Returns
@@ -44,4 +21,75 @@ fn spawn_app() -> String {
   let _ = tokio::spawn(server);
 
   format!("http://127.0.0.1:{}", port)
+}
+
+/// Test function to check if the health check endpoint works.
+#[tokio::test]
+async fn health_check_works() {
+  let address: String = spawn_app();
+  let client: Client = reqwest::Client::new();
+
+  // Send a GET request to the '/health_check' endpoint.
+  let response: Response = client
+    .get(&format!("{}/health_check", &address))
+    .send()
+    .await
+    .expect("Failed to execute request.");
+
+  // Assert that the response status is successful (2xx).
+  assert!(response.status().is_success());
+
+  // Assert that the response content length is 0 (no body).
+  assert_eq!(Some(0), response.content_length());
+}
+
+/// Test function to validate that subscribing with valid form data returns a 200 status code.
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+  let address: String = spawn_app();
+  let client:Client = reqwest::Client::new();
+  let body: &str = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+  // Send a POST request to the '/subscriptions' endpoint with valid form data.
+  let response: Response = client
+    .post(&format!("{}/subscriptions", &address))
+    .header("Content-Type", "application/x-www-form-urlencoded")
+    .body(body)
+    .send()
+    .await
+    .expect("Failed to execute request.");
+
+  // Assert that the response status is 200 OK.
+  assert_eq!(200, response.status().as_u16());
+}
+
+/// Test function to validate that subscribing with missing data returns a 400 status code.
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+  let address: String = spawn_app();
+  let client: Client = reqwest::Client::new();
+  let test_cases: Vec<(&str, &str)> = vec![
+    ("name=le%20guin", "missing the email"),
+    ("email=ursula_le_guin%40gmail.com", "missing the name"),
+    ("", "missing both name and email"),
+  ];
+
+  // Iterate over the test cases and send POST requests with missing data.
+  for (invalid_body, error_message) in test_cases {
+    let response: Response = client
+      .post(&format!("{}/subscriptions", &address))
+      .header("Content-Type", "application/x-www-form-urlencoded")
+      .body(invalid_body)
+      .send()
+      .await
+      .expect("Failed to execute request.");
+
+    // Assert that the response status is 400 Bad Request.
+    assert_eq!(
+      400,
+      response.status().as_u16(),
+      "The API did not fail with 400 Bad Request when the payload was {}.",
+      error_message
+    );
+  }
 }
