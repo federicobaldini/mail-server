2d81df8a58b93e436c559203c3fbb2d15fc7e84e
Federico Baldini <federico.baldini7@gmail.com>
2023-07-02 00:44:51 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,22 @@ use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 
+#[derive(serde::Deserialize)]
+struct FormData {
+  email: String,
+  name: String,
+}
+
 /// Handler function for the HTTP request on the '/health_check' route.
 ///
 /// This function handles the incoming HTTP request and returns a 200 OK response
 /// with no body, indicating that the server is healthy.
 async fn health_check(_: HttpRequest) -> HttpResponse {
+  HttpResponse::Ok().finish()
+}
+
+/// Handler function for the HTTP request on the '/subscriptions' route.
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
   HttpResponse::Ok().finish()
 }
 
@@ -22,10 +33,13 @@ async fn health_check(_: HttpRequest) -> HttpResponse {
 /// A `Result` containing the `Server` instance if the server started successfully,
 /// or an `std::io::Error` if an error occurred.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-  let server: Server =
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-      .listen(listener)? // Attempts to bind the TCP listener to the server.
-      .run(); // Starts the server.
+  let server: Server = HttpServer::new(|| {
+    App::new()
+      .route("/health_check", web::get().to(health_check))
+      .route("/subscriptions", web::post().to(subscribe))
+  })
+  .listen(listener)? // Attempts to bind the TCP listener to the server.
+  .run(); // Starts the server.
 
   // Run the server and return the result indicating the success or failure of server binding.
   Ok(server)
