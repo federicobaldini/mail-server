21cf7ccedaa52b6d89d0ff302c01ea0b664ed61a
Federico Baldini <federico.baldini7@gmail.com>
2023-07-16 13:22:53 +0200

    Update health_check.rs

--- tests/api/health_check.rs
+++ tests/api/health_check.rs
@@ -1,41 +1,26 @@
 use reqwest::{Client, Response};
 
 use crate::helpers::spawn_app;
-use mail_server::configuration::{get_configuration, Settings};
-use sqlx::{Connection, PgConnection};
 
 /// Test function to check if the health check endpoint works.
 #[tokio::test]
 async fn health_check_works() {
   // Spawn the application and retrieve the address
-  let app_address: String = spawn_app();
-
-  // Retrieve the configuration settings
-  let configuration: Settings = get_configuration().expect("Failed to read configuration");
-
-  // Build the connection string for the database
-  let connection_string: String = configuration.database.connection_string();
-
-  // Connect to the Postgres database
-  let connection: PgConnection = PgConnection::connect(&connection_string)
-    .await
-    .expect("Failed to connect to Postgres.");
+  let address: String = spawn_app();
 
   // Create a new reqwest client
   let client: Client = reqwest::Client::new();
 
-  // Prepare the request body
-  let body: &str = "name=federico%20baldini&email=federico_baldini%40gmail.com";
-
-  // Send a POST request to the subscriptions endpoint
+  // Send a GET request to the '/health_check' endpoint.
   let response: Response = client
-    .post(&format!("{}/subscriptions", &app_address))
-    .header("Content-Type", "application/x-www-form-urlencoded")
-    .body(body)
+    .get(&format!("{}/health_check", &address))
     .send()
     .await
     .expect("Failed to execute request.");
 
-  // Assert that the response status is 200
-  assert_eq!(200, response.status().as_u16());
+  // Assert that the response status is successful (2xx).
+  assert!(response.status().is_success());
+
+  // Assert that the response content length is 0 (no body).
+  assert_eq!(Some(0), response.content_length());
 }
