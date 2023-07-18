e82f6ba85883bf3e139a7380a9a34bcbef46a87b
Federico Baldini <federico.baldini7@gmail.com>
2023-07-18 19:03:05 +0200

    Update subscriptions.rs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
-use actix_web::{HttpResponse, web};
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,7 +9,36 @@ pub struct FormData {
   name: String,
 }
 
-/// Handler function for the HTTP request on the '/subscriptions' route.
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-  HttpResponse::Ok().finish()
+/// Handles the HTTP POST request for subscribing to a service.
+///
+/// ### Arguments
+///
+/// * `form` - A `web::Form` containing the form data submitted in the request body.
+/// * `pool` - A `web::Data` instance holding the PostgreSQL connection pool.
+///
+/// ### Returns
+///
+/// An `HttpResponse` indicating the success or failure of the subscription operation.
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+  // Execute an SQL query to insert the form data into the 'subscriptions' table.
+  match sqlx::query!(
+    r#"
+  INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
+  "#,
+    Uuid::new_v4(),
+    form.email,
+    form.name,
+    Utc::now()
+  )
+  .execute(pool.as_ref())
+  .await
+  {
+    // Return a successful HTTP response.
+    Ok(_) => HttpResponse::Ok().finish(),
+    // If there was an error executing the query, log the error and return an internal server error response.
+    Err(e) => {
+      println!("Failed to execute query: {}", e);
+      HttpResponse::InternalServerError().finish()
+    }
+  }
 }
