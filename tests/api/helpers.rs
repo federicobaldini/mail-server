f5c475541a1f49441f558aeab758763dae1848f4
Federico Baldini <federico.baldini7@gmail.com>
2023-07-22 07:16:30 +0200

    Update helpers.rs

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,24 +1,45 @@
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
-use mail_server::startup::run;
+use mail_server::{
+  configuration::{get_configuration, Settings},
+  startup::run,
+};
+use sqlx::{PgPool, Pool, Postgres};
 
-/// Spawns the application server and returns the server address.
-///
-/// ### Returns
-///
-/// A string representing the address of the spawned server, in the format `http://127.0.0.1:port`.
-pub fn spawn_app() -> String {
+pub struct TestApp {
+  pub address: String,
+  pub db_pool: PgPool,
+}
+
+/// Spawns the mail server application and returns a TestApp instance.
+pub async fn spawn_app() -> TestApp {
+  // Bind the TCP listener to a random available port.
   let listener: TcpListener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
   // Retrieve the port assigned to us by the OS.
   let port: u16 = listener.local_addr().unwrap().port();
 
+  let address: String = format!("http://127.0.0.1:{}", port);
+
+  // Load the application configuration from the environment or configuration file.
+  let configuration: Settings = get_configuration().expect("Failed to read configuration.");
+
+  // Connect to the Postgres database using the provided connection string.
+  let connection_pool: Pool<Postgres> =
+    PgPool::connect(&configuration.database.connection_string())
+      .await
+      .expect("Failed to connect to Postgres.");
+
   // Start the mail server application and bind it to the address.
-  let server: Server = run(listener).expect("Failed to bind address");
+  let server: Server = run(listener, connection_pool.clone()).expect("Failed to bind address");
 
   // Spawn the server on the tokio runtime in the background.
   let _ = tokio::spawn(server);
 
-  format!("http://127.0.0.1:{}", port)
+  // Create a TestApp instance with the address and connection pool.
+  TestApp {
+    address,
+    db_pool: connection_pool,
+  }
 }
